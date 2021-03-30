e2215f3e916ef158ee31228db240b5dce8d62c93
= <=>
2021-03-30 13:57:17 +0200

    Adds English Flair NER too

--- FlairAnonymizer.py
+++ FlairAnonymizer.py
@@ -15,14 +15,16 @@ flair.cache_root = Path("/data/cache/.flair")
 
 class FlairAnonymizer(Anonymizer):
     def __init__(self):
-        self.entities = [Entities.PER.name, Entities.LOC.name]
-        self.tagger = SequenceTagger.load("flair/ner-spanish-large")
+        self.entities = [Entities.PER.name, Entities.LOC.name, Entities.DATE.name, Entities.TIME.name, Entities.GPE.name]
+        self.taggers = [SequenceTagger.load("flair/ner-spanish-large"),
+                        SequenceTagger.load("flair/ner-english-ontonotes-large")]
         super().__init__()
 
     def anonymize(self, text):
-        sentence = Sentence(text)
-        self.tagger.predict(sentence)
-        ents = [(ent.to_original_text(), ent.tag) for ent in sentence.get_spans('ner') if ent.tag in self.entities]
-        for ent in ents:
-            text = text.replace(ent[0], ent[1])
+        for tagger in self.taggers:
+            sentence = Sentence(text)
+            tagger.predict(sentence)
+            ents = [(ent.to_original_text(), ent.tag) for ent in sentence.get_spans('ner') if ent.tag in self.entities]
+            for ent in ents:
+                text = text.replace(ent[0], ent[1])
         return text
