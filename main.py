c0c21d467f722914f76a4a2cf239b25f15d3c4ec
Jose J. Martinez <josejuan.martinez@iqvia.com>
2021-03-30 07:32:14 -0400

    Checks flair

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import os
 
 # from RupertaAnonymizer import RupertaAnonymizer
-# from FlairAnonymizer import FlairAnonymizer
+from FlairAnonymizer import FlairAnonymizer
 from SpacyAnonymizer import SpacyAnonymizer
 from constants import ONCOLOGY_DATA, OTHERS_DATA
 
 if __name__ == '__main__':
-    # anonymizer = FlairAnonymizer()
-    anonymizer = SpacyAnonymizer()
+    anonymizer = FlairAnonymizer()
+    # anonymizer = SpacyAnonymizer()
     txt = []
     for folder in (ONCOLOGY_DATA, OTHERS_DATA):
         for filename in os.listdir(folder):
