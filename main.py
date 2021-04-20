fd4cbfe20654b9559bc7e4f2eac4572ac19287c5
Jose J. Martinez <josejuan.martinez@iqvia.com>
2021-04-20 11:18:16 -0400

    Modifies main

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import os
 from RegexAnonymizer import RegexAnonymizer
 from Sentencizer import Sentencizer
 
-# from FlairAnonymizer import FlairAnonymizer
-# from SpacyAnonymizer import SpacyAnonymizer
+from FlairAnonymizer import FlairAnonymizer
+from SpacyAnonymizer import SpacyAnonymizer
 from constants import ONCOLOGY_DATA, OTHERS_DATA, OUT_DATA
 
 if __name__ == '__main__':
     sentencizer = Sentencizer()
     PHONEEMAIL_anonymizer = RegexAnonymizer()
-    # PERLOC_anonymizer = FlairAnonymizer()
-    # DATETIMEGPE_anonymizer = SpacyAnonymizer()
+    PERLOC_anonymizer = FlairAnonymizer()
+    DATETIMEGPE_anonymizer = SpacyAnonymizer()
 
     for folder in (ONCOLOGY_DATA, OTHERS_DATA):
         for filename in os.listdir(folder):
@@ -21,8 +21,8 @@ if __name__ == '__main__':
                 for sent in sentencizer.sentencize(f.read()):
                     sent_str = sent.text
                     sentxt = PHONEEMAIL_anonymizer.anonynimize(sent_str)
-                    # sentxt = PERLOC_anonymizer.anonymize(sentxt)
-                    # sentxt = DATETIMEGPE_anonymizer.anonymize(sentxt)
+                    sentxt = PERLOC_anonymizer.anonymize(sentxt)
+                    sentxt = DATETIMEGPE_anonymizer.anonymize(sentxt)
 
                     doc_sents.append(sentxt)
                 with open(os.path.join(folder, OUT_DATA, filename), 'w', encoding='utf-8') as fo:
